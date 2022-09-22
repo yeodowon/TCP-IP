79dfba5e70e59a98b5c81e3df8330b979e0113bd
yeodowon <ehdnjsdk@gmail.com>
2022-09-22 18:42:49 +0900

    first

---
+++ tcp_client/build/CMakeFiles/tcp_client.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for tcp_client.
+# This may be replaced when dependencies are built.
---
+++ tcp_server/build/CMakeFiles/tcp_server.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tcp_server.dir/tcp_server.c.o"
+  "CMakeFiles/tcp_server.dir/tcp_server.c.o.d"
+  "tcp_server"
+  "tcp_server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/tcp_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
